6680681b438ba4b1a72ff801625931eabef4036c
sahiljain9 <sahiljain9@gmail.com>
2026-07-17 03:36:35 +0530

    Added calculate_weekly_change() to features.py

--- Pipeline/features.py
+++ Pipeline/features.py
@@ -10,3 +10,12 @@ def calculate_daily_change(current_rate, previous_rate):
     if previous_rate == 0 or previous_rate is None:
         return 0.0
     return round(((current_rate - previous_rate) / previous_rate) * 100, 4)
+
+def calculate_weekly_change(current_rate, week_ago_rate):
+    """
+    Feature 2: Weekly Change %
+    Percentage change in exchange rate compared to 7 days ago.
+    """
+    if week_ago_rate == 0 or week_ago_rate is None:
+        return 0.0
+    return round(((current_rate - week_ago_rate) / week_ago_rate) * 100, 4)
